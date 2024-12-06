81974b8c82e6795a6a598847091e85dead690a00
NGastaPooh <romandm90@gmail.com>
2024-12-06 00:58:09 -0600

    fixed a problem with blank files; reorganised orca-related input data

---
+++ from_cluster/results_b3lyp/output_perturbations/pmc/TSAR_COPY.sh
@@ -0,0 +1,17 @@
+prepath="/uhpc/bittner/rdmitrie/ORCA/dg/dg_pmc/dirs"
+filename="std.out"
+cd dirs || exit
+for i in {1..108};do
+  cd "$i" || exit
+  for j in x y z;do
+    cd "$j" || exit
+    for k in plus minus ;do
+      cd "$k" || exit
+      path1="${prepath}/${i}/${j}/${k}"
+      cp "${path1}/${filename}" .
+      cd ..
+    done
+    cd ..
+  done
+  cd ..
+done
